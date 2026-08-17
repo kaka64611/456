e8dbbe9a0711d7f7a7a846ab52c2f3769ef0cc0a
RG52MINI Builder <rg52mini@example.com>
2026-08-17 14:05:48 +0800

    Fix mpv fatal error: remove invalid analyzeduration option, use --vo=gbm (drm disabled in build)

--- rg52mini-tv/src/player.c
+++ rg52mini-tv/src/player.c
@@ -80,13 +80,12 @@ bool player_load(TVPlayer *p, const char *url) {
     // Clear previous mpv log
     system("rm -f /roms/ports/rg52mini-tv/mpv.log");
 
-    // Build mpv command - force drm video output, increase analyze duration, disable OSD
+    // Build mpv command - use gbm video output (drm is disabled in this build)
     char cmd[2048];
     snprintf(cmd, sizeof(cmd),
-        "mpv --fs --vo=drm --ao=alsa --volume=%d --cache=yes --cache-secs=10 "
+        "mpv --fs --vo=gbm --ao=alsa --volume=%d --cache=yes --cache-secs=10 "
         "--network-timeout=20 --keep-open=always "
         "--force-window=yes --vd-lavc-threads=4 "
-        "--analyzeduration=10000000 --probesize=10000000 "
         "--osd-level=0 "
         "--input-gamepad=yes "
         "--input-conf=/roms/ports/rg52mini-tv/mpv-input.conf "
